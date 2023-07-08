74a451959ea5a09ea9b7c14a017d5b7a9666cd64
Vanex-h <vanessahirwa5@gmail.com>
2023-07-08 11:31:22 +0200

    Backend only

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const connectToDatabase = require("./utils/db.connection");
+const User = require("./models/user.model");
+const usersRoutes  =  require('./routes/users.routes')
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+const app = express();
+const PORT = 3200;
+
+// turn req json to javascript object
+app.use(express.json());
+
+// routing
+
+
+app.use('/', usersRoutes)
+
+connectToDatabase();
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
